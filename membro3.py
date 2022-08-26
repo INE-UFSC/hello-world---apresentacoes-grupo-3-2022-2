910f7123d88b5b57c06a3d6b37205262b67d986d
Vinicius <vinivinicius09@gmail.com>
2022-08-26 15:41:20 -0300

    adicionei mais informação

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o Vinicius, tenho 20 anos, sou natural de florianópolis, gosto de ver filmes, gosto de jogos de fps, curto muito futebol, <3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Vinicius, tenho 20 anos, sou natural de florianópolis, gosto de ver filmes, gosto de jogos de fps, curto muito futebol, não tenho muita experiência com programação, mas vou começar a investir mais tempo nesse quesito nesse semestre." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
