b49af55e9eec9b56513345ceceae580bacd87254
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-08-26 11:44:12 -0300

    Adicionada mensagem do membro Andre

--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return """Olá, eu sou o André Rocco! Tenho 18 anos e gosto de assistir séries, andar de skate, jogar tênis e jogos eletrônicos no meu tempo livre. 
+	Tenho experiência prévia de programação com Python (principalmente fazendo bots usando Selenium) e experiência com desenvolvimento web (HTML, CSS e JavaScript).
+	Na disciplina de Programação Orientada a Objeto II espero levar meu conhecimento de programação para o próximo nível, me tornar capaz de desenvolver sistemas que não sejam apenas CRUD, me tornando capaz de construir grandes projetos!""" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
