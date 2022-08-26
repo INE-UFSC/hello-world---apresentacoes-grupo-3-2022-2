3e1b3b02f18ef6d87e24e696d2633fa74466c84e
Ismael Heinzelmann <ismaelheinzelmann@gmail.com>
2022-08-26 11:09:56 -0300

    apresentação ismael

---
+++ membro4.py
@@ -0,0 +1,4 @@
+def hello():
+	return """Olá, eu sou o Ismael Coral, tenho 22 anos, sou estremamente curioso, por isso gosto de estudar coisas num geral, jogar coisas, ler assistir filmes e etc.
+    Tenho contato com programação à anos e sou extremamente ligado a àrea desde criança, e pretendo ligar a àrea de computação com a àrea de filosofia no futuro.
+    Desta matéria, espero ter exposição à experiência de criação de software com pessoas diferentes e pretendo conversar muito com os professores sobre o conteúdo."""#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
